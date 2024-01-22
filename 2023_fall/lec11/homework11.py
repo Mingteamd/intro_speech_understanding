8a9e8edc134ccfb800cc6db41c8cb001b77f73cc
Mingteamd <mingteamd@icloud.com>
2024-01-22 16:41:13 +0900

    Add files via upload

--- 2023_fall/lec11/homework11.py
+++ 2023_fall/lec11/homework11.py
@@ -1,16 +1,33 @@
-import speech_recognition
+import speech_recognition as sr
 
 def transcribe_wavefile(filename, language='en'):
     '''
     Use sr.Recognizer.AudioFile(filename) as the source,
     recognize from that source,
     and return the recognized text.
-    
+
     @params:
     filename (str) - the filename from which to read the audio
     language (str) - the language of the audio (optional; default is English)
-    
+
     @returns:
     text (str) - the recognized speech
     '''
-    raise RuntimeError("FAIL!!  You need to change this function so it works!")
+    recognizer = sr.Recognizer()
+
+    with sr.AudioFile(filename) as source:
+        recognizer.adjust_for_ambient_noise(source)
+
+        audio = recognizer.record(source)
+
+        try:
+            text = recognizer.recognize_google(audio, language=language)
+            return text
+        except sr.UnknownValueError:
+            print("Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print(f"Could not request results from Google Speech Recognition service; {e}")
+
+    return None
+
+
